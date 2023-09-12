11a714920b25685b70b9235cb3ff49ffc5558ce5
solomonbaez <solomon.james.baez@gmail.com>
2023-09-11 23:54:22 -0700

    Added basic health endpoint

--- api/main.go
+++ api/main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
 func main() {
-	fmt.Printf("Hello!\n")
+	router := gin.Default()
+
+	router.GET("/health", func(c *gin.Context) {
+		c.JSON(http.StatusOK, "hEaLtHy!")
+	})
+
+	router.Run(":8000")
 }
