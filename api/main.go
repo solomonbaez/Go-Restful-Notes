5fbb49f48c9bc17ca93b8bc055bded417dfe9cf9
solomonbaez <solomon.james.baez@gmail.com>
2023-09-12 11:58:19 -0700

    Added Note type with POST method

--- api/main.go
+++ api/main.go
@@ -5,10 +5,25 @@ import (
 	"net/http"
 )
 
+type Note struct {
+	Text string `json:"note"`
+}
+
 func main() {
 	router := gin.Default()
 
 	var notes []string
+
+	router.POST("/notes", func(c *gin.Context) {
+		var note Note
+		if e := c.ShouldBindJSON(&note); e != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": e.Error()})
+			return
+		}
+
+		notes = append(notes, note.Text)
+		c.Status(http.StatusCreated)
+	})
 
 	router.GET("/health", func(c *gin.Context) {
 		c.JSON(http.StatusOK, "hEaLtHy!")
