5b551b3b6365e730bd1451e3522dbe721645bdc8
solomonbaez <solomon.james.baez@gmail.com>
2023-09-12 10:53:32 -0700

    Added /notes endpoint

--- api/main.go
+++ api/main.go
@@ -8,8 +8,14 @@ import (
 func main() {
 	router := gin.Default()
 
+	var notes []string
+
 	router.GET("/health", func(c *gin.Context) {
 		c.JSON(http.StatusOK, "hEaLtHy!")
+	})
+
+	router.GET("/notes", func(c *gin.Context) {
+		c.JSON(http.StatusOK, notes)
 	})
 
 	router.Run(":8000")
