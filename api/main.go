6594f95e78e2d3a54ffb6f5f2f6ebf414dc66f10
solomonbaez <solomon.james.baez@gmail.com>
2023-09-12 21:55:18 -0700

    Switched to MySQL Database and implemented a GET method for * notes

--- api/main.go
+++ api/main.go
@@ -2,72 +2,80 @@ package main
 
 import (
 	"database/sql"
-	// "fmt"
+	"fmt"
+	"log"
 	"net/http"
-	"strconv"
 
 	"github.com/gin-gonic/gin"
-	// "github.com/go-sql-driver/mysql"
+	"github.com/go-sql-driver/mysql"
 )
 
 // db -> notes_api
 const (
 	DBUSER     = "mysql"
 	DBPASSWORD = "mysql"
-	DBHOST     = "localhost"
+	DBNET      = "tcp"
+	DBHOST     = "127.0.0.1:3306"
 	DBPORT     = "3306"
 	DBNAME     = "notes_api"
 )
 
 type Note struct {
-	ID    int    `json:"id"`
-	Title string `json:"title"`
-	Text  string `json:"content"`
+	ID      int    `json:"id"`
+	Title   string `json:"title"`
+	Content string `json:"content"`
 }
 
 var db *sql.DB
 
 func main() {
+	cfg := mysql.Config{
+		User:   DBUSER,
+		Passwd: DBPASSWORD,
+		Net:    DBNET,
+		Addr:   DBHOST,
+		DBName: DBNAME,
+	}
+
+	var e error
+	db, e = sql.Open("mysql", cfg.FormatDSN())
+	if e != nil {
+		log.Fatal(e)
+	}
+
+	var p error
+	p = db.Ping()
+	if p != nil {
+		log.Fatal(p)
+	}
+
+	fmt.Println("Database Connection: Success!")
+	defer db.Close()
+
 	router := gin.Default()
 
-	router.GET("/health", func(c *gin.Context) {
-		c.JSON(http.StatusOK, "hEaLtHy!")
-	})
-
-	router.GET("/notes", func(c *gin.Context) {
-		c.JSON(http.StatusOK, notes)
-	})
-
-	router.GET("/notes/:id", func(c *gin.Context) {
-		id_str := c.Param("id")
-		id, e := strconv.Atoi(id_str)
-		if e != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid ID"})
-			return
-		}
-
-		for _, note := range notes {
-			if note.ID == id {
-				c.JSON(http.StatusOK, note)
-				return
-			}
-		}
-
-		c.JSON(http.StatusNotFound, gin.H{"error": "Note not found"})
-	})
-
-	router.POST("/notes", func(c *gin.Context) {
-		var note Note
-		if e := c.ShouldBindJSON(&note); e != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": e.Error()})
-			return
-		}
-
-		note.ID = id_counter
-		id_counter++
-		notes = append(notes, note)
-		c.Status(http.StatusCreated)
-	})
+	router.GET("/notes", getNotes)
 
 	router.Run(":8000")
+}
+
+func getNotes(c *gin.Context) {
+	rows, e := db.Query("SELECT * FROM notes")
+	if e != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch notes"})
+		return
+	}
+
+	defer rows.Close()
+
+	var notes []Note
+	for rows.Next() {
+		var note Note
+		if e := rows.Scan(&note.ID, &note.Title, &note.Content); e != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to scan notes"})
+		}
+		notes = append(notes, note)
+	}
+
+	c.JSON(http.StatusOK, notes)
 }
