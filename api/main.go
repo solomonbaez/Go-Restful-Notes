299d5b3e39ad710d179c19581ac6279c4d3c354e
solomonbaez <solomon.james.baez@gmail.com>
2023-09-12 23:24:10 -0700

    Added GET method for /notes by id

--- api/main.go
+++ api/main.go
@@ -55,6 +55,7 @@ func main() {
 	router := gin.Default()
 
 	router.GET("/notes", getNotes)
+	router.GET("/notes/:id", getNoteByID)
 
 	router.Run(":8000")
 }
@@ -73,9 +74,26 @@ func getNotes(c *gin.Context) {
 		var note Note
 		if e := rows.Scan(&note.ID, &note.Title, &note.Content); e != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to scan notes"})
+			return
 		}
 		notes = append(notes, note)
 	}
 
 	c.JSON(http.StatusOK, notes)
+}
+
+func getNoteByID(c *gin.Context) {
+	id := c.Param("id")
+	var note Note
+
+	// populate note item if query is successful
+	e := db.QueryRow(
+		"SELECT id, title, content FROM notes WHERE id = ?", id,
+	).Scan(&note.ID, &note.Title, &note.Content)
+	if e != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Failed to fetch note"})
+		return
+	}
+
+	c.JSON(http.StatusOK, note)
 }
